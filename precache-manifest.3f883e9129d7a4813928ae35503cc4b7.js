d62210a85592ad4fdd755bf3d98bf03ec89a90f7
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2020-01-30 12:04:37 +0330

    Updates

---
+++ precache-manifest.3f883e9129d7a4813928ae35503cc4b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d2c6dca7bdb2ce38896b294c5310ba",
+    "url": "/ishan-admin/index.html"
+  },
+  {
+    "revision": "fc8a483568c5753ac828",
+    "url": "/ishan-admin/static/css/main.7323e003.chunk.css"
+  },
+  {
+    "revision": "173e93804baaa9f2517e",
+    "url": "/ishan-admin/static/js/2.71a23a08.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/ishan-admin/static/js/2.71a23a08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc8a483568c5753ac828",
+    "url": "/ishan-admin/static/js/main.8e4dd73f.chunk.js"
+  },
+  {
+    "revision": "849f57faf6efe804a08b",
+    "url": "/ishan-admin/static/js/runtime-main.c2bd7dd2.js"
+  }
+]);
